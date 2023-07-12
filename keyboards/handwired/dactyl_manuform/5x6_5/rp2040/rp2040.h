96ce84bfaf1049273d3a44a3eeb07bc7dee3936e
Angus Dippenaar <angusdippenaar@gmail.com>
2023-07-12 19:51:15 +0200

    it's still not working

---
+++ keyboards/handwired/dactyl_manuform/5x6_5/rp2040/rp2040.h
@@ -0,0 +1,9 @@
+// Copyright 2023 Angus (@angaz)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// #undef SOFT_SERIAL_PIN
+// #define SOFT_SERIAL_PIN GP0
+// #define SERIAL_USART_FULL_DUPLEX
+// #define SERIAL_USART_RX_PIN GP1
