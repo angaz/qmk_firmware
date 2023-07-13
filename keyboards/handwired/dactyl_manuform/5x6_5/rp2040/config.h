b9dc0ac74bba4638af4e2db65b420b7e54ddcc35
Angus Dippenaar <angusdippenaar@gmail.com>
2023-07-13 02:19:14 +0200

    It's working!

--- keyboards/handwired/dactyl_manuform/5x6_5/rp2040/config.h
+++ keyboards/handwired/dactyl_manuform/5x6_5/rp2040/config.h
@@ -3,7 +3,9 @@
 
 #pragma once
 
-#undef SOFT_SERIAL_PIN
-#define SOFT_SERIAL_PIN GP0
+// #undef SOFT_SERIAL_PIN
+// #define SOFT_SERIAL_PIN GP0
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_RX_PIN GP1
+
+#define EE_HANDS
